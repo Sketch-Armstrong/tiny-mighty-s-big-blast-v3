33e8cbc231829da741b73e9f41bd2708e0c8f2bd
Sketch-Armstrong <85261893+Sketch-Armstrong@users.noreply.github.com>
2026-08-20 19:37:43 -0400

    started adding selection arrow cursor to the menu's buttons, on focus

--- resources/menus/main menu/button_manager.gd
+++ resources/menus/main menu/button_manager.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+@onready var arrow_icon = $FocusArrow
+@onready var padding = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +14,28 @@ func _process(delta: float) -> void:
 	#if GlobalFocusBooleans.main_menu_focus == true:
 		#$".".grab_focus()
 	pass
+
+
+func _on_scores_focus_entered() -> void:
+	arrow_icon.visible = true
+	arrow_icon.position = $Scores.position
+
+
+func _on_start_focus_entered() -> void:
+	arrow_icon.visible = true
+	arrow_icon.position = $Start.position
+
+
+func _on_options_focus_entered() -> void:
+	arrow_icon.visible = true
+	arrow_icon.position = $Options.position
+
+
+func _on_quit_focus_entered() -> void:
+	arrow_icon.visible = true
+	arrow_icon.position = $Quit.position
+
+
+func _on_legal_focus_entered() -> void:
+	arrow_icon.visible = true
+	arrow_icon.position = $Legal.position
